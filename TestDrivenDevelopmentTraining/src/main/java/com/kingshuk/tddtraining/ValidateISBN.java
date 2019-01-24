309eed3b7a4f1d1543ccbbe4c82dafc6415ba988
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-01-23 21:18:06 -0600

    Further clean up in the implementation method

--- TestDrivenDevelopmentTraining/src/main/java/com/kingshuk/tddtraining/ValidateISBN.java
+++ TestDrivenDevelopmentTraining/src/main/java/com/kingshuk/tddtraining/ValidateISBN.java
@@ -4,23 +4,18 @@ public class ValidateISBN {
 
 	public boolean checkIsbn(String isbn) {
 
-		
 		if(isbn.length()!=10){
 			return false;
 		}
 		
 		char [] isbnArray = isbn.toCharArray();
 				
-		int multiplier = 10;
-		
-		int sumTotal = 0;
+				int sumTotal = 0;
 		
 		for (int i = 0; i < isbnArray.length; i++) {
-			int digit = Character.getNumericValue(isbnArray[i]);
-			
-			sumTotal += digit*multiplier;
-			
-			multiplier--;
+			//Just realized that we don't need to convert a character to an integer
+			//By any additional steps. Have to revisit data types in java, soon
+			sumTotal += isbnArray[i]*(isbnArray.length-i);
 		}
 		
 		return sumTotal%11==0;
