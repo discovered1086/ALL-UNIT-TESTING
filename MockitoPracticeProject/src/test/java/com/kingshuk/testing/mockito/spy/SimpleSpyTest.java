fafe8d8961ffee89582865bdd674fe0d12516a69
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-04-27 19:19:16 -0500

    New project created for practicing PowerMock

--- MockitoPracticeProject/src/test/java/com/kingshuk/testing/mockito/spy/SimpleSpyTest.java
+++ MockitoPracticeProject/src/test/java/com/kingshuk/testing/mockito/spy/SimpleSpyTest.java
@@ -1,7 +1,6 @@
 package com.kingshuk.testing.mockito.spy;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
